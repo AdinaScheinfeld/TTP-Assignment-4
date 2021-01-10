207818ea705b2dd33e9b46e8bcba08984dc97347
Adina Scheinfeld <Adina.Scheinfeld20@bcmail.cuny.edu>
2021-01-09 21:32:35 -0500

    adds comments to files

--- js/part1.js
+++ js/part1.js
@@ -1,12 +1,14 @@
 
+//event for I'm right button
 document.getElementsByTagName('button')[0]
     .addEventListener('click', function(){
         message = document.getElementById('message');
         message.innerText = "I'm right";
     });
 
-    document.getElementsByTagName('button')[1]
-        .addEventListener('click', function(){
-            message = document.getElementById('message');
-            message.innerText = "No, I'm right";
-    });
+//event for No, I'm right button
+document.getElementsByTagName('button')[1]
+    .addEventListener('click', function(){
+        message = document.getElementById('message');
+        message.innerText = "No, I'm right";
+});
--- js/part2.js
+++ js/part2.js
@@ -1,3 +1,5 @@
+
+//alert for scrolling over illegal area
 document.getElementById("illegalArea")
     .addEventListener('mouseover', function(event){
         alert('Hey, I told you not to hover over me!');
